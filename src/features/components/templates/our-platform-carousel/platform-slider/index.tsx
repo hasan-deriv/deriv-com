61f8dda93de5239420653c1105ff8ce6495123e2
hasan-deriv <abuhasan@deriv.com>
2023-07-18 16:16:28 +0800

    feat: added width, height and flex css to slide

--- src/features/components/templates/our-platform-carousel/platform-slider/index.tsx
+++ src/features/components/templates/our-platform-carousel/platform-slider/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect, useMemo } from 'react'
 import type { Dispatch, SetStateAction } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import type { EmblaOptionsType } from 'embla-carousel-react'
-import styled, { css } from 'styled-components'
 import Autoplay from 'embla-carousel-autoplay'
 import { PlatformContent, ImageTag, PLATFORMS_CAROUSEL_DELAY } from '../_utils'
 import type { TPlatformDetails } from '../_utils'
@@ -18,20 +17,10 @@ import {
     shadow_end,
     box_wrapper,
 } from './platform-slider.module.scss'
-import { Box } from 'components/containers'
 import { Header } from 'components/elements'
-import device from 'themes/device'
 import useRegion from 'components/hooks/use-region'
 import dclsx from 'features/utils/dclsx'
 import Flex from 'features/components/atoms/flex-box'
-
-// const SelectedZone = styled(Flex)`
-//     left: 0;
-//     right: 0;
-//     box-shadow: rgba(131, 131, 131, 0.15) 0 16px 20px, rgba(131, 131, 131, 0.15) 0 0 20px;
-//     top: calc(50% - 152px / 2);
-//     z-index: 4;
-// `
 
 type SelectedSlideProps = {
     selected_slide: TPlatformDetails
@@ -58,65 +47,6 @@ const SelectedSlide = ({ selected_slide }: SelectedSlideProps) => {
 
     return <></>
 }
-
-const Shadow = styled.div<{ location: 'start' | 'end' }>`
-    position: absolute;
-    left: 0;
-    right: 0;
-    height: calc(50% - 152px / 2);
-    z-index: 1;
-    pointer-events: none;
-    ${({ location }) => {
-        if (location === 'start') {
-            return css`
-                top: -0.5px;
-                background: linear-gradient(
-                    to top,
-                    rgba(249, 251, 255, 0) 0%,
-                    rgba(249, 251, 255, 1) 100%
-                );
-            `
-        } else {
-            return css`
-                bottom: -0.5px;
-                background: linear-gradient(rgba(249, 251, 255, 0) 0%, rgba(249, 251, 255, 1) 100%);
-            `
-        }
-    }}
-`
-
-// const Scene = styled.div`
-//     min-width: 100%;
-//     height: 100%;
-//     overflow: hidden;
-// `
-
-// const Viewport = styled(Flex)`
-
-// `
-
-// const WheelContainer = styled.div`
-//     height: 100px;
-//     width: 96%;
-//     perspective: 1000px;
-//     perspective-origin: 150%;
-// `
-
-const Slide = styled(Flex)<{ distance_center: number }>`
-    text-align: center;
-    align-items: center;
-    cursor: pointer;
-    backface-visibility: hidden;
-    margin-bottom: 40px;
-`
-
-const StyledFlex = styled(Flex)`
-    width: 600px;
-
-    @media ${device.desktop} {
-        width: 384px;
-    }
-`
 
 type PlatformSliderProps = {
     slide_index: number
@@ -195,6 +125,8 @@ const PlatformSlider = ({ slide_index, onSelectSlide, platform_details }: Platfo
                                             key={learn_more_link}
                                             onClick={() => clickHandler(index)}
                                             className={slide}
+                                            align="center"
+                                            justify="start"
                                         >
                                             <ImageTag src={icon} />
                                             <Header type="subtitle-1">{title}</Header>
